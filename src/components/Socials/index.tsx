08e5639a42b96a45356c7f68bf5a1a069dc68dde
asus <108582867+gartnoire@users.noreply.github.com>
2023-03-06 17:28:22 +0400

    Added: config for pritter, logos for social networks

--- src/components/Socials/index.tsx
+++ src/components/Socials/index.tsx
@@ -1,13 +1,48 @@
-import React from "react";
+import React from 'react';
+import cn from 'classnames';
+
+import styles from './index.module.scss';
 
 interface SocialsProps {}
 
+const socialNetworks = [
+  {
+    id: 1,
+    title: 'Discord',
+    className: 'Discord',
+    link: 'https://discord.com/',
+  },
+  {
+    id: 2,
+    title: 'Twitter',
+    className: 'Twitter',
+    link: 'https://twitter.com/',
+  },
+  {
+    id: 3,
+    title: 'Medium',
+    className: 'Medium',
+    link: 'https://medium.com/',
+  },
+];
+
 export const Socials: React.FC<SocialsProps> = ({}) => {
   return (
-    <ul>
-      <li></li>
-      <li></li>
-      <li></li>
+    <ul className={styles.socialList}>
+      {socialNetworks.map((social) => (
+        <li key={social.id} className={styles.socialListItem}>
+          <a
+            href={social.link}
+            target="_blank"
+            rel="noreferrer"
+            className={cn(
+              styles.socialListLink,
+              styles[`socialList${social.className}`]
+            )}>
+            {social.title}
+          </a>
+        </li>
+      ))}
     </ul>
   );
 };
